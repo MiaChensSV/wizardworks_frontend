414b75e94816759fa320e7bf73011357ab3efcde
Mia Chen <miachensv@gmail.com>
2024-10-19 20:31:17 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="app">
       <header className="app-header">
-        <div className="page-root">
+        <div>
           <Desk />
         </div>
       </header>
--- src/util/https.js
+++ src/util/https.js
@@ -3,72 +3,72 @@ const BASE_URL = 'https://localhost:7017/api/Block';
 
 const http = () => {
 
-    const get = async (endpoint, options = {}) => {
-        try {
-            const response = await fetch(`${BASE_URL}${endpoint}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    ...options.headers,
-                },
-                ...options,
-            });
+	const get = async (endpoint, options = {}) => {
+		try {
+			const response = await fetch(`${BASE_URL}${endpoint}`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+					...options.headers,
+				},
+				...options,
+			});
 
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status} ${response.statusText}`);
-            }
+			if (!response.ok) {
+				throw new Error(`Error: ${response.status} ${response.statusText}`);
+			}
 
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.error('GET request failed:', error);
-            throw error;
-        }
-    };
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			console.error('GET request failed:', error);
+			throw error;
+		}
+	};
 
-    // Function to handle POST requests
-    const post = async (endpoint, body) => {
-        try {
-            const response = await fetch(`${BASE_URL}${endpoint}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(body),
-            });
-            console.log(response)
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status} ${response.statusText}`);
-            }
+	// Function to handle POST requests
+	const post = async (endpoint, body) => {
+		try {
+			const response = await fetch(`${BASE_URL}${endpoint}`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(body),
+			});
+			console.log(response)
+			if (!response.ok) {
+				throw new Error(`Error: ${response.status} ${response.statusText}`);
+			}
 
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.error('POST request failed:', error);
-            throw error; // Re-throw error for further handling
-        }
-    };
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			console.error('POST request failed:', error);
+			throw error; // Re-throw error for further handling
+		}
+	};
 
-    const remove = async (endpoint) => {
-        try {
-            const response = await fetch(`${BASE_URL}${endpoint}`, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status} ${response.statusText}`);
-            }
-            return response.status;
-        }
-        catch (error) {
-            console.error('Delete request failed:', error)
-            throw error;
-        }
-    }
+	const remove = async (endpoint) => {
+		try {
+			const response = await fetch(`${BASE_URL}${endpoint}`, {
+				method: 'DELETE',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+			if (!response.ok) {
+				throw new Error(`Error: ${response.status} ${response.statusText}`);
+			}
+			return response.status;
+		}
+		catch (error) {
+			console.error('Delete request failed:', error)
+			throw error;
+		}
+	}
 
-    return { get, post, remove };
+	return { get, post, remove };
 };
 
 export default http;
